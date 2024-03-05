6b9d3f8f1e42259ac371d1197fe639769a60fa1c
Eoin Houlihan <eoin.m.houlihan@gmail.com>
2024-03-05 17:51:37 +0000

    Use cursor moved events to rotate ship to face cursor

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ struct Scoreboard {
     score: usize,
 }
 
+#[derive(Resource, Default)]
+struct MouseWorldCoords(Vec2);
+
 #[derive(Component)]
 struct ScoreboardUi;
 
@@ -26,11 +29,13 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(Scoreboard { score: 0 })
+        .init_resource::<MouseWorldCoords>()
         .add_systems(Startup, setup)
         .add_systems(
             Update,
             (
-                (mouse_click_system, update_scoreboard).chain(),
+                (cursor_moved, player_rotation).chain(),
+                (mouse_click, update_scoreboard).chain(),
                 bevy::window::close_on_esc,
             ),
         )
@@ -77,7 +82,35 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
 }
 
-fn mouse_click_system(
+fn cursor_moved(
+    mut mouse_world_coords: ResMut<MouseWorldCoords>,
+    mut cursor_event_reader: EventReader<CursorMoved>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+
+    for event in cursor_event_reader.read() {
+        // TODO: Should this be ignoring everything but the last event? Is there a better way to update this?
+        if let Some(world_position) = camera.viewport_to_world_2d(camera_transform, event.position)
+        {
+            mouse_world_coords.0 = world_position;
+        }
+    }
+}
+
+fn player_rotation(
+    mouse_world_coords: Res<MouseWorldCoords>,
+    mut player_transform_query: Query<&mut Transform, With<Player>>,
+) {
+    let mut player_transform = player_transform_query.single_mut();
+
+    let to_mouse_world_coords =
+        (mouse_world_coords.0 - player_transform.translation.xy()).normalize_or_zero();
+
+    player_transform.rotation = Quat::from_rotation_arc(Vec3::Y, to_mouse_world_coords.extend(0.0));
+}
+
+fn mouse_click(
     mouse_button_input: Res<ButtonInput<MouseButton>>,
     mut scoreboard: ResMut<Scoreboard>,
     window_query: Query<&Window, With<PrimaryWindow>>,
